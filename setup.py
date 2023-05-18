af53f6896e63aa8a08225c73233a7e1bf3cfb9b6
Igor Korotchenkov <idebugall@gmail.com>
2023-05-18 18:12:26 +0400

    Compatibility fix for NetBox 3.5

--- setup.py
+++ setup.py
@@ -7,9 +7,9 @@ with open(path.join(top_level_directory, 'README.md'), encoding='utf-8') as file
 
 setup(
     name='phonebox_plugin',
-    version='v0.0.5-beta.1',
+    version='v0.0.6-beta.1',
     url='https://github.com/iDebugAll/phonebox-plugin.git',
-    download_url='https://github.com/iDebugAll/phonebox-plugin/archive/v0.0.5-beta.1.tar.gz',
+    download_url='https://github.com/iDebugAll/phonebox-plugin/archive/v0.0.6-beta.1.tar.gz',
     description='A phone numbers management plugin for NetBox.',
     long_description=long_description,
     long_description_content_type='text/markdown',
