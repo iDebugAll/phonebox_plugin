d245328837f8158aa067e07e1982392392a61a04
Igor Korotchenkov <idebugall@gmail.com>
2024-06-12 17:49:18 +0400

    Compatibility fix for NetBox 3.7

--- phonebox_plugin/__init__.py
+++ phonebox_plugin/__init__.py
@@ -5,7 +5,7 @@ class PhoneBoxConfig(PluginConfig):
     name = 'phonebox_plugin'
     verbose_name = 'PhoneBox Plugin'
     description = 'Telephone Number Management Plugin for NetBox.'
-    version = 'v0.0.7-beta.2'
+    version = 'v0.0.8'
     author = 'Igor Korotchenkov'
     author_email = 'iDebugAll@gmail.com'
     base_url = 'phonebox'
--- phonebox_plugin/urls.py
+++ phonebox_plugin/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path("", views.NumberListView.as_view(), name="list_view"),
+    path("number_list/", views.NumberListView.as_view(), name="number_list"),
     path("number/<int:pk>/", views.NumberView.as_view(), name="number_view"),
     path("add_number/", views.NumberEditView.as_view(), name="add_number"),
     path('import_numbers/', views.NumberBulkImportView.as_view(), name='import_numbers'),
@@ -13,6 +14,7 @@ urlpatterns = [
     path("number_bulk_delete/", views.NumberBulkDeleteView.as_view(), name="number_bulk_delete"),
     path("voice_circuit/<int:pk>/", views.VoiceCircuitView.as_view(), name="voice_circuit_view"),
     path("voice_circuit_list_view/", views.VoiceCircuitListView.as_view(), name="voice_circuit_list_view"),
+    path("voicecircuit_list/", views.VoiceCircuitListView.as_view(), name="voicecircuit_list"),
     path("add_voice_circuit/", views.VoiceCircuitEditView.as_view(), name="add_voice_circuit"),
     path('import_voice_circuits/', views.VoiceCircuitBulkImportView.as_view(), name='import_voice_circuits'),
     path("voice_circuit/<int:pk>/edit/", views.VoiceCircuitEditView.as_view(), name="voice_circuit_edit"),
